ff2b658c7593a77df6d829447c5af6d44f9c489d
Lyle Mantooth <lyle.mantooth@primasupply.com>
2016-08-01 16:47:16 -0400

    Maintain separations of host vs. site name.
    Format all the command strings!

--- functional_tests/server_tools.py
+++ functional_tests/server_tools.py
@@ -5,19 +5,19 @@ THIS_FOLDER = path.dirname(path.abspath(__file__))
 
 SSH_HOST = '192.168.1.74'
 
-def create_session_on_server(host, email):
+def create_session_on_server(site_name, email):
     return subprocess.check_output(
         [
             'fab',
-            'create_session_on_server:site_name={},email={}'.format(host, email),
+            'create_session_on_server:site_name={},email={}'.format(site_name, email),
             '--hosts={}'.format(SSH_HOST),
             '--hide=everything,status',
         ],
         cwd=THIS_FOLDER
     ).decode().strip()
 
-def reset_database(host):
+def reset_database(site_name):
     subprocess.check_call(
-        ['fab', 'reset_database:site_name={}', '--hosts={}'.format(host, SSH_HOST)],
+        ['fab', 'reset_database:site_name={}'.format(site_name), '--hosts={}'.format(SSH_HOST)],
         cwd=THIS_FOLDER
     )
